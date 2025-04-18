176e79baeb8e5136b902fa0757b6c69bfe1d797b
Yanko Aleksandrov <yanko@idrobots.com>
2025-04-18 19:53:06 +0300

    encoders

---
+++ installer/video_encoders/video_software_encoder.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+gst-launch-1.0 -e \
+    rtspsrc location=rtsp://192.168.144.25:8554/main.264 latency=50 drop-on-latency=true ! \
+    rtph264depay ! h264parse ! \
+    nvv4l2decoder enable-max-performance=1 disable-dpb=1 ! \
+    video/x-raw\(memory:NVMM\),format=NV12 ! \
+    nvvidconv ! \
+    video/x-raw,format=I420 ! \
+    videorate max-rate=25 ! \
+    x264enc tune=zerolatency speed-preset=ultrafast bitrate=2500 key-int-max=15 bframes=0 ! \
+    h264parse config-interval=1 ! \
+    video/x-h264,stream-format=byte-stream,alignment=au ! \
+    udpsink host=127.0.0.1 port=5010 sync=false
